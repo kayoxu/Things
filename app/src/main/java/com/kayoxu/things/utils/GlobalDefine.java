48b51982ce35a764f9603cbf8dcab35ed0e8f2b9
KayoXu <371714752@qq.com>
2017-09-07 10:17:23 +0800

    demo

--- app/src/main/java/com/kayoxu/things/utils/GlobalDefine.java
+++ app/src/main/java/com/kayoxu/things/utils/GlobalDefine.java
@@ -1,5 +1,7 @@
 package com.kayoxu.things.utils;
 
+import com.kayoxu.things.BuildConfig;
+
 /**
  * Created by KayoXu on 2017/9/6.
  * 全局变量
@@ -7,9 +9,9 @@ package com.kayoxu.things.utils;
 
 public class GlobalDefine {
 
+    public static boolean debug = BuildConfig.DEBUG;
+
     public static final String ACTION_INTENT_GENERAL = "com.kayoxu.things.broadcast.general";
-
-
 
 
 }
--- app/src/main/java/com/kayoxu/things/utils/LogUtil.java
+++ app/src/main/java/com/kayoxu/things/utils/LogUtil.java
@@ -1,22 +1,17 @@
 package com.kayoxu.things.utils;
 
-
 import android.util.Log;
 
-import com.kayoxu.things.BuildConfig;
 
 public class LogUtil {
-
-    private static boolean debug = BuildConfig.DEBUG;
-
     public static void d(String tag, String msg) {
-        if (debug) {
+        if (GlobalDefine.debug) {
             Log.d(tag, msg);
         }
     }
 
     public static void e(String tag, String msg) {
-        if (debug) {
+        if (GlobalDefine.debug) {
             Log.e(tag, msg);
         }
     }
